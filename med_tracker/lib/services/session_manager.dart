3ea0448358f2717c5391d5f6401b814d8c3e3e0d
tsaniamagistra <123210090@student.upnyk.ac.id>
2024-05-21 00:37:35 +0700

    add session manager

---
+++ med_tracker/lib/services/session_manager.dart
@@ -0,0 +1,36 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SessionManager {
+  static final String _isLoggedInKey = '';
+  static final String _userIdKey = '';
+
+  // Set login status
+  static Future<void> setLoggedIn(bool isLoggedIn) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_isLoggedInKey, isLoggedIn);
+  }
+
+  // Get login status
+  static Future<bool> isLoggedIn() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_isLoggedInKey) ?? false;
+  }
+
+  // Set userId
+  static Future<void> setuserId(String userId) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_userIdKey, userId);
+  }
+
+  // Get userId
+  static Future<String?> getuserId() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_userIdKey);
+  }
+
+  // Clear all session data
+  static Future<void> logout() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+}
