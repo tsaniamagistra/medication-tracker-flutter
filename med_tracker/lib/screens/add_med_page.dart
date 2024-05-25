4bfb887a8e14acb2b350631267f7cabfd817829a
tsaniamagistra <123210090@student.upnyk.ac.id>
2024-05-25 12:04:41 +0700

    add inputFormatter

--- med_tracker/lib/screens/add_med_page.dart
+++ med_tracker/lib/screens/add_med_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:med_tracker/api/data_source.dart';
 import 'package:med_tracker/models/medicine.dart';
+import 'package:med_tracker/screens/home_page.dart';
 import 'package:med_tracker/services/session_manager.dart';
 
 class AddMedicinePage extends StatefulWidget {
@@ -9,6 +11,7 @@ class AddMedicinePage extends StatefulWidget {
 }
 
 class _AddMedicinePageState extends State<AddMedicinePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _dosageController = TextEditingController();
   final TextEditingController _frequencyController = TextEditingController();
@@ -29,96 +32,129 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Medicine Name'),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _dosageController,
-              decoration: InputDecoration(labelText: 'Dosage'),
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _frequencyController,
-                    decoration: InputDecoration(labelText: 'Frequency'),
-                    keyboardType: TextInputType.number,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Medicine Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Medicine name cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _dosageController,
+                decoration: InputDecoration(labelText: 'Dosage'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Dosage cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _frequencyController,
+                      decoration: InputDecoration(labelText: 'Frequency'),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Frequency cannot be empty';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(width: 16.0),
-                Text(
-                  '/',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                SizedBox(width: 16.0),
-                DropdownButton<String>(
-                    value: _selectedFrequencyType ?? _frequencyTypes.first,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedFrequencyType = value;
-                      });
-                    },
-                    items: _frequencyTypes
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList()),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _additionalInfoController,
-              decoration: InputDecoration(labelText: 'Additional Info'),
-              maxLines: 3,
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _priceController,
-                    decoration: InputDecoration(labelText: 'Price'),
-                    keyboardType: TextInputType.number,
+                  SizedBox(width: 16.0),
+                  Text(
+                    '/',
+                    style: TextStyle(fontSize: 20.0),
                   ),
-                ),
-                SizedBox(width: 16.0),
-                Text(
-                  '/',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                SizedBox(width: 16.0),
-                DropdownButton<String>(
-                    value: _selectedCurrency ?? _currencies.first,
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedCurrency = value;
-                      });
-                    },
-                    items: _currencies
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList()),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                _addMedicine();
-              },
-              child: Text('Add Medicine'),
-            ),
-          ],
+                  SizedBox(width: 16.0),
+                  DropdownButton<String>(
+                      value: _selectedFrequencyType ?? _frequencyTypes.first,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedFrequencyType = value;
+                        });
+                      },
+                      items: _frequencyTypes
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _additionalInfoController,
+                decoration: InputDecoration(labelText: 'Additional Info'),
+                maxLines: 3,
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _priceController,
+                      decoration: InputDecoration(labelText: 'Price'),
+                      keyboardType:
+                      TextInputType.numberWithOptions(decimal: true),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^\d+\.?\d{0,2}')),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Text(
+                    '/',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  SizedBox(width: 16.0),
+                  DropdownButton<String>(
+                      value: _selectedCurrency ?? _currencies.first,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedCurrency = value;
+                        });
+                      },
+                      items: _currencies
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _addMedicine();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  }
+                },
+                child: Text('Add Medicine'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -129,9 +165,9 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
     String name = _nameController.text.trim();
     String dosage = _dosageController.text.trim();
     int? frequency = int.tryParse(_frequencyController.text.trim());
-    String? frequencyType = _selectedFrequencyType;
-    double? price = double.tryParse(_frequencyController.text.trim());
-    String? currency = _selectedCurrency;
+    String? frequencyType = _selectedFrequencyType ?? _frequencyTypes.first;
+    double? price = double.tryParse(_priceController.text.trim());
+    String? currency = _selectedCurrency ?? _currencies.first;
     String additionalInfo = _additionalInfoController.text.trim();
 
     if (userId != null) {
@@ -163,7 +199,6 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
           ),
         );
       });
-
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
