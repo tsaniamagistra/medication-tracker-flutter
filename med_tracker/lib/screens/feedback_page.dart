40acd73e1a55276a1a31265117e1242be282066d
tsaniamagistra <123210090@student.upnyk.ac.id>
2024-05-27 22:17:17 +0700

    add feedback page

--- med_tracker/lib/screens/feedback_page.dart
+++ med_tracker/lib/screens/feedback_page.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:med_tracker/widgets/bottom_navbar.dart';
 
 class FeedbackPage extends StatelessWidget {
-  const FeedbackPage({super.key});
+  const FeedbackPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Feedback'),
+      ),
+      bottomNavigationBar: BottomNavBar(selectedIndex: 2),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                margin: EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Saran:',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5.0),
+                      Text(
+                        'Durasi presentasi per kelompok dapat dibatasi, sehingga masih tersedia pertemuan yang cukup untuk penyampaian materi teori.',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Card(
+                margin: EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Kesan:',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5.0),
+                      Text(
+                        'Tugas-tugas yang diberikan menantang, mendorong mahasiswa untuk melakukan eksplorasi mengenai Flutter.',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
